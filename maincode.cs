dd7389a770d8da33876dd19604f80d63f55263b2
Lutz Terra <162689393+jardonshore@users.noreply.github.com>
2024-03-08 11:26:13 +0200

    Create maincode.cs

---
+++ maincode.cs
@@ -0,0 +1,29 @@
+  public static UsableType[] GetUsableTypeArray (Type[] types, params UsableType[] additionalUsableTypes)
+        {
+            List<UsableType> usableTypeList = new List<UsableType> ();
+            for (int i = 0; i < types.Length; i++)
+            {
+                usableTypeList.Add (new UsableType (types[i]));
+            }
+            usableTypeList.AddRange (additionalUsableTypes);
+            return usableTypeList.ToArray ();
+        }
+
+        public static UsableType[] AmalgamateUsableTypes (UsableType[] usableTypeArray, params UsableType[] usableTypes)
+        {
+            List<UsableType> usableTypeList = new List<UsableType> ();
+            for (int i = 0; i < usableTypes.Length; i++)
+            {
+                usableTypeList.Add (usableTypes[i]);
+            }
+            usableTypeList.AddRange(usableTypeArray);
+            return usableTypeList.ToArray();
+        }
+
+public class LightControlBehaviour : PlayableBehaviour
+{
+    public Color color = Color.white;
+    public float intensity = 1f;
+    public float bounceIntensity = 1f;
+    public float range = 10f;
+}
